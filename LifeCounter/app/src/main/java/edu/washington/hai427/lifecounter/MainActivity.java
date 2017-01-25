0366e0c206fe3d1f23487afc0a4a4bb9ee588416
Hai Nguyen <hai427@uw.edu>
2017-01-24 19:43:46 -0800

    buttons work, need to implement losing

--- LifeCounter/app/src/main/java/edu/washington/hai427/lifecounter/MainActivity.java
+++ LifeCounter/app/src/main/java/edu/washington/hai427/lifecounter/MainActivity.java
@@ -2,12 +2,53 @@ package edu.washington.hai427.lifecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView[] players = new TextView[4];
+    TextView[] lifeCounterLabels = new TextView[4];
+    Button[] buttons = new Button[16];
+    int[] counters = new int[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for (int i=0; i<4; i++) {
+            counters[i] = 20;
+        }
+
+        for (int i=0; i<4; i++) {
+            String lifeLabelID = "life" + (i + 1);
+            int resID = getResources().getIdentifier(lifeLabelID, "id", getPackageName());
+            lifeCounterLabels[i] = (TextView) (findViewById(resID));
+            lifeCounterLabels[i].setText("Life Counter: " + counters[i] + "");
+        }
+    }
+
+    public void adjustLifeCounter(View v){
+        Button button = (Button) v;
+        int playerId = Integer.parseInt(button.getTag().toString()) - 1;
+        String text = button.getText().toString();
+        if(text.equals("+")){
+            counters[playerId]++;
+            lifeCounterLabels[playerId].setText("Life Counter: " + counters[playerId]);
+        }
+        if(text.equals("-")){
+            counters[playerId]--;
+            lifeCounterLabels[playerId].setText("Life Counter: " + counters[playerId]);
+        }
+        if(text.equals("+5")){
+            counters[playerId]+=5;
+            lifeCounterLabels[playerId].setText("Life Counter: " + counters[playerId]);
+        }
+        if(text.equals("-5")) {
+            counters[playerId]-=5;
+            lifeCounterLabels[playerId].setText("Life Counter: " + counters[playerId]);
+        }
     }
 }
